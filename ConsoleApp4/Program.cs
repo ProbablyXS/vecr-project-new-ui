74056b76470d98d5c7cfe6f1a54a52c4cb7c03ad
Admin <ProbablyXS>
2025-05-29 22:31:10 +0200

    1.2

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -229,6 +229,7 @@ class Program
                         case ".jpg":
                         case ".jpeg": contentType = "image/jpeg"; break;
                         case ".gif": contentType = "image/gif"; break;
+                        case ".svg": contentType = "image/svg+xml"; break;
                         default: contentType = "application/octet-stream"; break;
                     }
 
